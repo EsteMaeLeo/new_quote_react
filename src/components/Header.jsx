e56889573818ddbf59b7cb6f35a9aa13cf5ec49d
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-02-09 22:34:52 -0600

    initial dev

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <h1 className="text-4xl font-extrabold text-gray-500 text-center">
+      How much <span className="text-indigo-700">Money</span> do you need?
+    </h1>
+  );
+}
+
+export default Header;
