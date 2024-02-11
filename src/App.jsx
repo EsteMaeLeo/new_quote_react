094bda6d3e698dfae61e3a982dedbda76f32ff47
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-02-10 21:17:26 -0600

    initial dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
+
 import Header from "./components/Header"
 
 function App() {
 
+  const [quantity, setQuantity] = useState(10000);
+  console.log(quantity)
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
+
+      <input 
+        type="range"
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-700"
+      />
     </div>
   )
 }
