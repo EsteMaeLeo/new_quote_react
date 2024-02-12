fbca51083c4dc34b737e9da8e084aeb7c5fecd63
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-02-11 22:48:20 -0600

    new changes dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import Header from "./components/Header"
+import Header from "./components/Header";
 
 function App() {
-
   const [quantity, setQuantity] = useState(10000);
-  console.log(quantity)
 
-  function handleChange(e){
-    console.log(e.target.value)
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+  console.log(quantity);
+
+  function handleChange(e) {
+    setQuantity(parseInt(e.target.value));
   }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
 
-      <input 
+      <input
         type="range"
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-700"
-        onChange={handleChange} 
+        onChange={handleChange}
+        min={MIN}
+        max={MAX}
+        step={STEP}
+        value={quantity}
       />
+      <p className="text-center my-10 text-5xl font-extrabold text-indigo-700">
+        {quantity}
+      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
