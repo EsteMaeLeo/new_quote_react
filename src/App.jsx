131aae99480363b264bc42713140dfc7288f1e01
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-02-13 00:05:40 -0600

    new changes dev

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,45 @@ function App() {
     setQuantity(parseInt(e.target.value));
   }
 
+  function handleClickMinus() {
+    const value = quantity - STEP;
+
+    if (value < MIN) {
+      alert("Quantity not valid");
+      return;
+    }
+
+    setQuantity(value);
+  }
+
+  function handleClickAdd() {
+    const value = quantity + STEP;
+
+    if (value > MAX) {
+      alert("Quantity not valid");
+      return;
+    }
+
+    setQuantity(value);
+  }
+
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
-      <div className="flex justify-between my-14">
+      <div className="flex justify-between my-6">
         <button
           type="button"
           className="h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-600 rounded-full hover:outline-none hover:ring-4 hover:ring-offset-2 hover:ring-lime-600"
+          onClick={handleClickMinus}
         >
           -
+        </button>
+        <button
+          type="button"
+          className="h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-600 rounded-full hover:outline-none hover:ring-4 hover:ring-offset-2 hover:ring-lime-600"
+          onClick={handleClickAdd}
+        >
+          +
         </button>
       </div>
       <input
